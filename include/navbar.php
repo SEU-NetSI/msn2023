7d1a9184b43c87b5d0e6c430bafcac100ac38604
LIN Chengdong <chengdlin2@ad.cityu.edu.hk>
2022-04-03 21:20:14 +0800

    clear info of 2021

--- include/navbar.php
+++ include/navbar.php
@@ -27,7 +27,7 @@
     <a href="index.php" style="margin-left: 1em;"><img style="width:80%;margin-top: 1em;"src="images/resources/logo.png"  alt="MSN 2021" /></a>
   </div>
   <div class="ui-block-b" style="width:55%;height:100%; font-weight: bold;font-size:1.5em;margin-top: 13px">
-    The 17th International Conference on Mobility, Sensing and Networking (MSN 2021)<br><span style="font-weight: normal;font-size:0.8em">December 13-15, 2021, Exeter, UK</span>
+    The 17th International Conference on Mobility, Sensing and Networking (MSN 2022)<br><span style="font-weight: normal;font-size:0.8em"><!-- December 13-15, 2021, Exeter, UK --></span>
   </div>
   <div class="ui-block-b" style="width:25%;height:100%; text-align: center">
     <span style="font-weight: bold">Technically Co-sponsored by IEEE</span>
@@ -44,10 +44,10 @@
       <div class="dropdown">
         <a class = "mybar-item dropbtn <?php if($mypage == "cf-papers.php" || $mypage == "cf-workshops.php" || $mypage == "cf-wkpaper.php" || $mypage == "cf-posters.php"){echo 'btn-selected-ui';}?>"href="#" >Calls</a>
         <div class="dropdown-content">
-          <a href="cf-papers.php">Call for Papers</a>
+<!--           <a href="cf-papers.php">Call for Papers</a>
           <a href="cf-workshops.php">Call for Workshops</a>
           <a href="cf-wkpaper.php">Call for Workshop Papers</a>
-          <a href="cf-posters.php">Call for Posters</a>
+          <a href="cf-posters.php">Call for Posters</a> -->
 <!--           <a href="camera_ready.php">Camera Ready</a> -->
         </div>
       </div>
@@ -56,21 +56,21 @@
       <div class="dropdown">
         <a class = "mybar-item <?php if ($mypage == "organization.php" || $mypage == "progcom.php") echo('btn-selected-ui') ?>" href="#">Committees</a>
           <div class="dropdown-content">
-            <a href="organization.php">Organization Committee</a>
-            <a href="progcom.php">Technical Program Committee</a>
+<!--             <a href="organization.php">Organization Committee</a>
+            <a href="progcom.php">Technical Program Committee</a> -->
           </div>
     </li>
     <li>
       <div class="dropdown">
           <a class = "mybar-item dropbtn <?php if($mypage == "keynote_1.php" ||$mypage == "keynote_2.php"|| $mypage == "workshop.php" || $mypage == "panel.php" || $mypage == "accepted-papers.php" || $mypage == "guide.php" || $mypage == "glance.php" || $mypage == "tech.php" || $mypage == "guide_chair.php" || $mypage == "tutorials.php"){echo 'btn-selected-ui';}?>"href="#" >Program</a>
           <div class="dropdown-content">
-            <a href="tutorials.php">Tutorials</a>
+<!--             <a href="tutorials.php">Tutorials</a>
             <a href="accepted-papers.php">List of Accepted Papers&nbsp;&nbsp;</a>
             <a href="panel.php">Panel&nbsp;&nbsp;</a>
             <a href="keynote_1.php">Keynote I&nbsp;&nbsp;</a>
             <a href="keynote_2.php">Keynote II&nbsp;&nbsp;</a>
             <a href="guide.php">Virtual Presentation Guidelines&nbsp;&nbsp;</a>
-            <a href="glance.php">Program at a Glance&nbsp;&nbsp;</a>
+            <a href="glance.php">Program at a Glance&nbsp;&nbsp;</a> -->
 <!--             <a href="tech.php">Technical Program&nbsp;&nbsp;</a>
             
             <a href="guide_chair.php">Guidelines for Session Chairs&nbsp;&nbsp;</a> -->
@@ -79,7 +79,7 @@
     </li>
     <li>
       <div class="dropdown">
-        <a class = "mybar-item dropbtn <?php if($mypage == "plat.php" || $mypage == "acco.php"){echo 'btn-selected-ui';}?>"href="plat.php" >Conference Virtual Platform</a>
+        <a class = "mybar-item dropbtn <?php if($mypage == "plat.php" || $mypage == "acco.php"){echo 'btn-selected-ui';}?>"href=# >Conference Virtual Platform</a>
         <div class="dropdown-content">
 <!--           <a href="venue.php">Conference Venue</a>
           <a href="acco.php">Accommodation</a> -->
@@ -109,16 +109,16 @@
       </div>
       <div style="width:100%;height:100px">
       <div  class="ui-block-a" style="width:22%;height:100%;background-color: #00aeef;padding-top:40px;">
-        <a href="cf-papers.php" style="color: white;font-size: 1.2em">Call for Papers >></a>
+        <a href="cf-papers.php" style="color: white;font-size: 1.2em"><!-- Call for Papers >> --></a>
       </div>
       <div  class="ui-block-b" style="width:26%;height:100%;background-color:rgb(95,148,235);padding-top:40px;">
-        <a href="cam-submission.php" style="color: white;font-size: 1.2em">Camera Ready Submission >></a>
+        <a href="cam-submission.php" style="color: white;font-size: 1.2em"><!-- Camera Ready Submission >> --></a>
       </div>
             <div  class="ui-block-b" style="width:22%;height:100%;background-color:#58595b;padding-top:40px;">
-        <a href="submission.php" style="color: white;font-size: 1.2em">Submission >></a>
+        <a href="submission.php" style="color: white;font-size: 1.2em"><!-- Submission >> --></a>
       </div>
       <div  class="ui-block-b" style="width:30%;height:100%;background-color:#6d6e70;padding-top:40px;">
-        <a href="registration.php" style="color: white;font-size: 1.2em">Registration >></a>
+        <a href="registration.php" style="color: white;font-size: 1.2em"><!-- Registration >> --></a>
       </div>
       </div>
 <?php } ?>
