b93400c62b3cfaa41945f06f77ac36fc984ffacd
LIN Chengdong <chengdlin2@ad.cityu.edu.hk>
2022-06-09 21:04:08 +0800

    tpc members for all tracks

--- progcom.php
+++ progcom.php
@@ -12,6 +12,18 @@
             add_listitem("<b>Track Chairs:</b>","","");
             add_listitem("Armir Bujari", "University of Bologna, Italy", "");
             add_listitem("Enrico Natalizio", "Technology Innovation Institute, UAE (Université de Lorraine, France)", "");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Pietro Manzoni", "Universitat Politecnica de Valencia, Spain", "");
+            add_listitem("Claudio E. Palazzi", "University of Padua, Italy", "");
+            add_listitem("Francesca Righetti", "University of Pisa, Italy", "");
+            add_listitem("Carlos Calafate", "Universitat Politecnica de Valencia, Spain", "");
+            add_listitem("Mirco Musolesi",  "University College London - University of Bologna, United Kingdom", "");
+            add_listitem("Johann Marquez Barja", "University of Antwerpen - imec, Belgium", "");
+            add_listitem("Eirini Eleni Tsiropoulou", "University of New Mexico Albuquerque, USA", "");
+            add_listitem("Merve Saimler", "Ericsson, Turkey", "");
+            add_listitem("Fabio D'Andreagiovanni", "French National Center for Scientific Research, France", "");
+            add_listitem("Francesco Betti Sorbelli", "University of Perugia, Italy", "");
       ?>
 
       <li data-role="list-divider">Track 2: Edge Computing, IoT and Digital Twins</li>
@@ -19,6 +31,33 @@
             add_listitem("<b>Track Chairs:</b>","","");
             add_listitem("Eirini Eleni Tsiropoulou", "University of New Mexico Albuquerque, USA", "");
             add_listitem("Suzan Bayhan", "University of Twente, Netherlands", "");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_lisitem("Mohammad Arif Ul Alam", "University of Massachusetts Lowell","");
+            add_lisitem("Atakan Aral", "University of Vienna","");
+            add_lisitem("David Bermbach", "TU Berlin","");
+            add_lisitem("Luiz F. Bittencourt", "University of Campinas","");
+            add_lisitem("Valeria Cardellini", "University of Roma \"Tor Vergata\"","");
+            add_lisitem("Lorenzo Corneo", "Aalto University","");
+            add_lisitem("Luca Davoli", "University of Parma","");
+            add_lisitem("Hao Han", "Nanjing University of Aeronautics & Astronautics","");
+            add_lisitem("Seyyedali Hosseinalipour", "Purdue University","");
+            add_lisitem("Yanqiu Huang", "University of Twente","");
+            add_lisitem("Chandra Krintz", "UC Santa Barbara","");
+            add_lisitem("Aris Leivadeas", "École de technologie supérieure","");
+            add_lisitem("Sanglu Lu", "State Key Laboratory for Novel Software Technology, Nanjing University, China","");
+            add_lisitem("Rolando Martins", "University of Porto","");
+            add_lisitem("Nathalie Mitton", "INRIA","");
+            add_lisitem("Nitinder Mohan", "Technical University Munich (TUM)","");
+            add_lisitem("Kwangsung Oh", "University of Nebraska at Omaha","");
+            add_lisitem("Symeon Papavassiliou", "National Technical University of Athens","");
+            add_lisitem("Carlo Puliafito", "University of Pisa","");
+            add_lisitem("Olga Saukh", "TU Graz / CSH Vienna","");
+            add_lisitem("Eleni Stai", "ETH Zurich","");
+            add_lisitem("Chiu Tan", "Temple University","");
+            add_lisitem("Yutao Tang", "Guilin University of Electronic and Technology","");
+            add_lisitem("Qiong Wu", "College of William and Mary","");
+            add_lisitem("Hong Zhang", "Hong Kong University of Science and Technology","");
       ?>
 
       <li data-role="list-divider">Track 3: Security, Privacy, Trust, and Blockchain</li>
@@ -26,6 +65,22 @@
             add_listitem("<b>Track Chairs:</b>","","");
             add_listitem("Weizhi Meng", "Technical University of Denmark, Denmark","");
             add_listitem("Sokratis K. Katsikas", "Norwegian University of Science and Technology, Norway","");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Shengshan Hu", "Huazhong University of Science and Technology","");
+            add_listitem("Yifeng Zheng", "Harbin Institute of Technology","");
+            add_listitem("Qianhong Wu", "Beihang University","");
+            add_listitem("Xingliang Yuan", "Monash University","");
+            add_listitem("Jiangshan Yu", "Monash University","");
+            add_listitem("Eric Chan-Tin", "Loyola University Chicago","");
+            add_listitem("Rhongho Jang", "Wayne State University","");
+            add_listitem("Lingchen Zhao", "Wuhan University","");
+            add_listitem("Wenjuan Li", "Hong Kong Polytechnic University","");
+            add_listitem("Daehun Nyang", "Ewha Womans University","");
+            add_listitem("Joongheon Kim", "Korea University","");
+            add_listitem("Josef Pieprzyk", "CSIRO/Data61","");
+            add_listitem("Francesco Buccafurri", "Mediterranea University of Reggio Calabria","");
+            add_listitem("Xuyun Zhang", "Macquarie University","");
       ?>
 
       <li data-role="list-divider">Track 4: Big Data and AI</li>
@@ -33,7 +88,17 @@
             add_listitem("<b>Track Chairs:</b>","","");
             add_listitem("Lorenzo Valerio", "IIT-CNR, Italy","");
             add_listitem("Giovanni Neglia", "INRIA, France","");
-          
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Robert Birke", "ABB Corporate Research","");
+            add_listitem("Makram Bouzid", "NOKIA Bell Labs","");
+            add_listitem("Pietro Manzoni", "Universitat Politècnica de València","");
+            add_listitem("Nitinder Mohan", "Technical University Munich","");
+            add_listitem("Ashwin Rao", "University of Helsinki","");
+            add_listitem("Stefano Savazzi", "IEIIT-CNR","");
+            add_listitem("Anna Maria Vegni", "Roma Tre University, Italy","");
+            add_listitem("Richard Vidal", "Accenture Technology Labs","");
+            add_listitem("Liang Xiao", "Xiamen University","");         
       ?>
 
       <li data-role="list-divider">Track 5: Systems, Tools, Testbed</li>
@@ -42,6 +107,39 @@
             add_listitem("James Zheng", "Macquarie University, Australia","");
             add_listitem("Lei Pan", "Deakin University, Australia","");
 
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("James Zheng", "Macquarie University, Australia", "");
+            add_listitem("Lei Pan", "Deakin University, Australia", "");
+            add_listitem("Charalambos Konstantinou", "King Abdullah University of Science and Technology, SA", "");
+            add_listitem("Ziwei Wang", "CSIRO, Australia", "");
+            add_listitem("Yangzhe Liao","Wuhan University of Technology, China", "");
+            add_listitem("Daishi Kondo","Osaka Prefecture University, Japan", "");
+            add_listitem("Wansu Lim", "Kumoh National Institute of Technology, Korea", "");
+            add_listitem("Xiaojun Zhai", "University of Essex, UK", "");
+            add_listitem("Hongyuan Jing", "Beijing Union University, China", "");
+            add_listitem("Yi Ren", "University of East Anglia, UK", "");
+            add_listitem("Wangyang Yu", "Shaanxi Normal University, China", "");
+            add_listitem("Faycal Mohaisen", "Qatar University, Qatar", "");
+            add_listitem("John Panneerselvam","University of Leicester, UK", "");
+            add_listitem("Keiichi Shima", "Nara Institute of Science and Technology, Japan", "");
+            add_listitem("Kofi Appiah", "University of York, UK", "");
+            add_listitem("Fanlin Meng", "University of Essex, UK", "");
+            add_listitem("Sangeet Saha", "University of Essex, UK", "");
+            add_listitem("Yongjun Zheng", "University of Hertfordshire, UK", "");
+            add_listitem("Yassine Himeur", "Qatar University, Qatar", "");
+            add_listitem("Xingzhen Bai", "Shandong University of Science and Technology, China", "");
+            add_listitem("Bin Ye", "China University of Mining and Technology, China", "");
+            add_listitem("Amit Singh", "University of Essex, UK", "");
+            add_listitem("Abdelkrim Boukabou", "University of Jijel, Algeria", "");
+            add_listitem("Sirajum Munir", "Bosch Research and Technology Center, USA", "");
+            add_listitem("Bo Yuan", "University of Leicester, UK", "");
+            add_listitem("Nathalie Mitton", "INRIA, France", "");
+            add_listitem("Khalida Ghanem", "CDTA, Algeria", "");
+            add_listitem("James Hardy", "University of Derby, UK", "");
+            add_listitem("Adel Oulefki", "CDTA, Algeria", "");
+            add_listitem("Minsi Chen", "University of Huddersfield, UK", "");
+            add_listitem("Chao Chen", "James Cook University, Australia", "");
+            add_listitem("Yanjun Zhang", "University of Queensland, Australia", "");
       ?>
 
       <li data-role="list-divider">Track 6: Applications in Smart Cities, Healthcare and Other Areas</li>
@@ -50,6 +148,39 @@
             add_listitem("Deze Zeng", "China University of Geosciences, China","");
             add_listitem("Zakirul Alam Bhuiyan", "Fordham University, USA","");
 
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Antonino Rullo", "University of Calabria", "");
+            add_listitem("Fei Hui", "Chang'an Univ", "");
+            add_listitem("Weifeng Sun", " Dalian University of Technology", "");
+            add_listitem("Xiaobo Zhou", "Tianjin University", "");
+            add_listitem("Tie Qiu", "Tianjin University", "");
+            add_listitem("Pankaj Mishra", "G b pant university of agricultural and technology", "");
+            add_listitem("Liu Sheng-Lan", "Dalian University of technology", "");
+            add_listitem("He Huang", "Soochow University", "");
+            add_listitem("Chao Fang", "Beijing University of Technology", "");
+            add_listitem("Luca Davoli", "University of Parma", "");
+            add_listitem("Ruiting Zhou", "Wuhan University", "");
+            add_listitem("Zhitao Guan", "North China Electric Power University", "");
+            add_listitem("Zhaolong Ning", "CQUPT", "");
+            add_listitem("Xiaoyan Yin", "Northwest University", "");
+            add_listitem("En Wang", "Jilin University", "");
+            add_listitem("Hyunbum Kim", "Incheon National University", "");
+            add_listitem("Yin Zhang", "University of Electronic Science and Technology of China", "");
+            add_listitem("Xiangjie Kong", "Zhejiang University of Technology", "");
+            add_listitem("Hao Zhou", "University of Science and Technology of China", "");
+            add_listitem("Sahil Verma", "Chandigarh University, Mohali", "");
+            add_listitem("Chunsheng Zhu", "The University of British Columbia", "");
+            add_listitem("Chi Lin", "School of Software, Dalian University of Technology", "");
+            add_listitem("Xiaohui Peng", "Institute of Computing Technology, Chinese Academy of Sciences", "");
+            add_listitem("Abdelkhalick Mohammad", "Rolls-Royce UTC, University of Nottingham", "");
+            add_listitem("Houbing Song", "Embry-Riddle Aeronautical University", "");
+            add_listitem("Gabriel Gomes de Oliveira", "UNICAMP", "");
+            add_listitem("Qiang He", "Department of Computing Technologies, Swinburne University of Technology", "");
+            add_listitem("Yuanfang Chen,Hangzhou Dianzi University", "");
+            add_listitem("Zahra Movahedi", "University of Tehran", "");
+            add_listitem("Haisheng Tan", "University of Science and Technology of China", "");
+            add_listitem("Yuan Wu", "University of Macau", "");
+            add_listitem("Rui Wang", "The Southern University of Science and Technology", "");
       ?>
 
     </ul>
