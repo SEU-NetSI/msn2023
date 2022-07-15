79d3ee44930641242b21231bc2845725bd8df4ff
LIN Chengdong <chengdlin2@ad.cityu.edu.hk>
2022-07-15 16:18:14 +0800

    firm

--- include/dates.php
+++ include/dates.php
@@ -1,6 +1,7 @@
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
     <?php add_dateitem("<del>Jul 1, 2022</del>", "<del>Paper Submission Due</del>"); ?>
-    <?php add_dateitem("<span style=\"color: red;font-weight: bold\">July 15, 2022</span>", "<span style=\"color: red;font-weight: bold\">Paper Submission Due(extended)</span>"); ?>
+    <?php add_dateitem("<del><span style=\"color: red;font-weight: bold\">July 15, 2022</span></del>", "<del><span style=\"color: red;font-weight: bold\">Paper Submission Due(extended)</span></del>"); ?>
+    <?php add_dateitem("<span style=\"color: red;font-weight: bold\">July 31 (AoE), 2022</span>", "<span style=\"color: red;font-weight: bold\">Paper Submission Due(Firm)</span>"); ?>
     <?php add_dateitem("Sep 15, 2022", "Author Notification"); ?>
     <?php add_dateitem("Oct 15, 2022", "Camera-Ready Due"); ?>
     <?php add_dateitem("Dec 14-16, 2022", "Conference Date"); ?> 
