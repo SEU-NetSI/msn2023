40512d43841a6fdd3fa6e5f32527a01cd11d7e7c
LIN Chengdong <chengdlin2@ad.cityu.edu.hk>
2022-09-03 13:09:00 +0800

    test

--- progcom.php
+++ progcom.php
@@ -5,8 +5,8 @@
 <div data-role="content" style="text-align:left;padding-top: 0em;background-color:#e4e5e6">
   <div  class="ui-block-a" style="width:70%;height:100%;background-color:#ffffff;padding-left: 35px;padding-right:35px;padding-top: 15px;">
     <h1 style="color: black">Technical Program Committee</h1>
+    
     <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="tbl">
-
       <li data-role="list-divider">Track 1: Mobile & Wireless Sensing and Networking</li>
       <?php
             add_listitem("<b>Track Chairs:</b>","","");
@@ -211,7 +211,6 @@
             add_listitem("Zhaolong Ning", "CQUPT", "");
             add_listitem("Zhitao Guan", "North China Electric Power University", "");
       ?>
-
     </ul>
   </div>
   <div  class="ui-block-b" style="width:30%;height:100%;background-color:#e4e5e6;padding:20px 15px 15px 15px">
