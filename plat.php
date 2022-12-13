960f6ad39053ce1e9accf5e372262dbd57a41591
LIN Chengdong <chengdlin2@ad.cityu.edu.hk>
2022-12-13 10:34:53 +0800

    update

--- plat.php
+++ plat.php
@@ -6,45 +6,44 @@
     <div  class="ui-block-a" style="width:70%;height:100%;background-color:#ffffff;padding-left: 35px;padding-right:35px;padding-top: 15px;">
         <h1>Virtual Conference Platform</h1>
         <p>Registered attendees can access the MSN 2021 virtual platform, by visiting <a href="https://msn2022.info">https://msn2022.info</a>.</p>
-            <h1>Virtual Conference Platform Guidelines</h1>
+            <br>
             <p>Dear Participants/Presenters of&nbsp;MSN&nbsp;2022,</p>
             <p>&nbsp;</p>
             <p>We look forward to (virtually) seeing you at IEEE&nbsp;MSN&nbsp;2022.</p>
-            <p>&nbsp;</p>
+
             <p>MSN 2022 will run virtually at&nbsp;<a href="https://msn2022.info/">https://msn2022.info</a>, and registered participants could access the papers/slides/videos/Zoom sessions in the conference and post comment in the Discussion of each session.</p>
             <p>The sessions are held via the Zoom meeting system and all links of Zoom meetings can be found at msn2022.info.</p>
-            <p>&nbsp;</p>
+
             <p>Please carefully read the following instructions on how to attend the virtual event. (In Particular, you need to pay attention to&nbsp;<strong>NO-SHOW&nbsp;Policy</strong>.)</p>
-            <p>&nbsp;</p>
+
             <p>&nbsp;- Signing up: If you have not yet done so, visit the online conference website (<a href="https://msn2022.info/">https://msn2022.info</a>) and sign up for a new account with your confirmation number in your IEEE receipt. If you have already signed up without a confirmation number or if you already have an account from&nbsp;MSN&nbsp;2022, please go to your profile and add a valid confirmation number. (Confirmation number can only be got through registration.)</p>
-            <p>&nbsp;</p>
+
             <p>- If you have just registered and your confirmation number is not yet found in the system, send an email message to&nbsp;<a href="mailto:support@duetone.com">support@duetone.com</a>. The website will be updated and new registrations will be added on a daily basis.</p>
-            <p>&nbsp;</p>
+
             <p>- Activate your account: Follow the link in the email message you receive after signing up to activate your account. You can also visit your profile and click the "Activate this account" button and an email message will be sent to you with an activation link.</p>
-            <p>&nbsp;</p>
+
             <p>- If you are an&nbsp;author&nbsp;or speaker of a paper, you have the obligation to attend the session where your talk will appear and answer questions from&nbsp;attendees&nbsp;of the conference. To enter the Zoom meeting for a particular session, go to the day and the session, and click the "Enter Zoom" button.</p>
             <p><strong>***If all the&nbsp;authors&nbsp;of one paper are absent in the paper session, this paper will be excluded from the proceedings according to IEEE &ldquo;no show&rdquo; policy.</strong></p>
-            <p>&nbsp;</p>
+
             <p>- There will be approximately 2 minutes at the end of each talk for the Q&amp;A period (<strong>Full </strong><strong>Papers:&nbsp;20&nbsp;min Presentation&nbsp;+ 2 min&nbsp;Q&amp;A,&nbsp;Workshop Papers: 10 min Presentation&nbsp;+ 2 min&nbsp;Q&amp;A,&nbsp;Short Papers: 10 min Presentation&nbsp;+ 2 min&nbsp;Q&amp;A</strong>).</p>
-            <p>&nbsp;</p>
+
             <p>If you are an audience, you can ask questions at that time. For&nbsp;authors, when answering questions from the audience, please unmute your microphone and speak directly. There will be a 2-minute count-down during the Q&amp;A, and the count-down will be paused by the host of the meeting if you need more time to answer all the questions.</p>
-            <p>&nbsp;</p>
+
             <p>The program at glance of IEEE&nbsp;MSN&nbsp;2022 is as follows.</p>
             <p><a href="https://msn2022.info/msn22/msn22-program-at-a-glance.pdf">https://msn2022.info/msn22/msn22-program-at-a-glance.pdf</a></p>
             <p><a href="https://ieee-msn.org/2022/">https://ieee-msn.org/2022/</a></p>
             <p>It has arranged with&nbsp;<strong>3</strong><strong> keynote speeches, 1 panel, </strong><strong>24</strong><strong> sessions, 5 workshops</strong>&nbsp;</p>
-            <p><strong>&nbsp;</strong></p>
+
             <p><strong>Dec. 14, 2022:&nbsp;</strong><br /> Keynotes:</p>
             <p><strong>Keynote 1:</strong>&nbsp;Understanding and Pushing the Sensing Limits of WiFi/4G/5G Signals</p>
             <p>Speaker:<strong>&nbsp;</strong><strong>Prof. Daqing Zhang </strong>(PKU and IP Paris)</p>
             <p><strong>Keynote 2:&nbsp;</strong>Toward Virtualized Edge Computing</p>
             <p>Speaker:&nbsp;<strong>Prof. Falko Dressler</strong> (TU Berlin)</p>
-            <p>&nbsp;</p>
+
             <p><strong>Dec. 15, 2022:&nbsp;</strong></p>
             <p><strong>Keynote 3:</strong> Real-Time AI for Infrastructure-assisted Autonomous Driving</p>
             <p>Speaker: <strong>Prof. Guoliang Xing</strong> (CUHK)</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
+
             <p><strong>Panel: Mobile sensing in AI age</strong></p>
             <p>Panel Chair:&nbsp;<strong>Prof. Huadong Ma&nbsp;</strong>(BUPT)</p>
             <p>Panel Members:</p>
@@ -53,13 +52,13 @@
             <li><strong>Prof. </strong><strong>Xinbing Wang </strong>(SJTU)</li>
             <li><strong>Prof. John C.S. Lui </strong>(CUHK)</li>
             </ul>
-            <p><strong>&nbsp;</strong>&nbsp;</p>
+
             <p>Wishing you an enjoyable conference.</p>
-            <p>&nbsp;</p>
+
             <p>Sincerely,</p>
             <p>Giuseppe Anastasi, Weigang Wu</p>
             <p>IEEE&nbsp;MSN&nbsp;2022 TPC Chairs</p>
-            <p>&nbsp;</p>
+            <p>&nbsp;</p><br><br>
 
     </div>
 
