cc9a414f4a67914601b3d9c89df94188f7fdf76d
Iven0322 <1584594639@qq.com>
2023-05-28 19:25:19 +0800

    加粗了committee内的标题字体

--- progcom.php
+++ progcom.php
@@ -10,14 +10,14 @@
       
       <li data-role="list-divider">Track 1: Mobile & Wireless Sensing and Networking </li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Jiliang Wang", "Tsinghua University, China", "");
       add_listitem("Debashri Roy", "University of Texas Arlington", "");
       ?>
 
       <li data-role="list-divider">Track 2: Edge Computing, IoT and Digital Twins </li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Xu Chen", "Sun Yat-sen University, China", "");
       add_listitem("Posco Tso", "Loughborough University, UK", "");
       ?>
@@ -25,7 +25,7 @@
       
       <li data-role="list-divider">Track 3: Security, Privacy, Trust, and Blockchain </li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Cliff C. Zou", "University of Central Florida, USA", "");
       add_listitem("Xiaoyu Ji", "Zhejiang University, China", "");
       ?>
@@ -33,21 +33,21 @@
       
       <li data-role="list-divider">Track 4: Big Data and AI</li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Tingjian Ge", "University of Massachusetts Lowell, USA", "");
       add_listitem("Tao Xiang", "Chongqing University, China", "");
       ?>
      
       <li data-role="list-divider">Track 5: Systems, Tools and Testbed </li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Kui Wu", "University of Victoria, Canada", "");
       add_listitem("Xiaolong Zheng", "Beijing University of Posts and Telecommunications, China", "");
       ?>
 
       <li data-role="list-divider">Track 6: Smart Cities, Metaverse and Applications </li>
       <?php
-       add_listitem("Track Chairs:", "", "");
+       add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Zichuan Xu", "Dalian University of Technology, China", "");
       add_listitem("Yue Zhang", "Drexel University, USA", "");
       ?>
