ca4bcd9f901f1758e861ce32058b1370d65a66d9
Iven0322 <1584594639@qq.com>
2023-06-16 10:51:17 +0800

    修改了workshop内容

--- cf-workshops.php
+++ cf-workshops.php
@@ -5,18 +5,13 @@
 
 <div data-role="content" style="text-align:left;padding-top: 0em;background-color:#e4e5e6">
     <div  class="ui-block-a" style="width:70%;height:100%;background-color:#ffffff;padding-left: 35px;padding-right:35px;padding-top: 15px;">
-        <b><h2>The 19th International Conference on Mobility, Sensing and Networking (MSN 2023)</h2>
-        <p>December 14-16, 2023, Nanjing, China</p></b>
-        <a href="https://ieee-msn.org/2023">https://ieee-msn.org/2023</a>
+
         <h1>Call for Workshops</h1>
  
 <p>Workshop proposals should be submitted (in PDF format) no later than Sep.15,2023 by e-mail to the MSN 2023 Worshop Co-chairs(see email id below),with "MSN 2023 Workshop Proposal" in the subject.Send your proposals to Haipeng Dai at haipengdai@nju.edu.cn or Tian Wang at tianwang@bnu.edu.cn</a>.</p>
 
         
 
-
-        <h2>Proposal Submission Guidelines</h2>
-        <p>TBD</p>
 
         <ol>
             
@@ -25,7 +20,7 @@
 
         <h2>Important Dates</h2>
        <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
-		    <?php add_dateitem("Sep.15,2023", "Workshop Proposal submitted Due"); ?>
+		    <?php add_dateitem("Sep.15,2023", "Workshop Proposal Submission Due"); ?>
 
 		</ul>
 
--- include/news.php
+++ include/news.php
@@ -1,12 +1,12 @@
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
 
 <?php
-  add_dateitem("June 11, 2023", "The paper submission system has been launched.");
+  add_dateitem("Jun 11, 2023", "The paper submission system has been launched.");
   add_dateitem("May 27, 2023", "Call for Papers published.");
   add_dateitem("May 25, 2023", "Web site is up.");
  
-  // add_dateitem("April 6, 2020", "<a href=\"cf-workshops.php\">Call for Workshops</a> published. ");
-  // add_dateitem("June 15, 2019", "CFP and call for workshops (CFW) out");
+  // add_dateitem("Apr 6, 2020", "<a href=\"cf-workshops.php\">Call for Workshops</a> published. ");
+  // add_dateitem("June15, 2019", "CFP and call for workshops (CFW) out");
 ?>
 
   <li class="newslibtn ui-btn ui-li" data-theme="a" onclick="showall();"><div><a>Older News</a><span class="ui-icon ui-btn-icon-right ui-icon-plus ui-icon-shadow">&nbsp;</span></div></li>
