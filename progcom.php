b3b08cf1852fb085f66b3d0ec0450870f1b3fe04
Iven0322 <1584594639@qq.com>
2023-07-07 08:10:36 +0800

    add the PC members

--- progcom.php
+++ progcom.php
@@ -12,7 +12,30 @@
       <?php
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Jiliang Wang", "Tsinghua University, China", "");
-      add_listitem("Debashri Roy", "University of Texas Arlington", "");
+      add_listitem("Debashri Roy", "University of Texas Arlington,USA", "");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Jiliang Wang", "Tsinghua University, China", "");
+add_listitem("Debashri Roy", "University of Texas Arlington, USA", "");
+            add_listitem("Xiangyu Xu", "Southeast University, China", "");
+            add_listitem("Haipeng Dai", "Nanjing University, China", "");
+            add_listitem("Chi Lin", "Dalian University of Technology, China", "");
+            add_listitem("Pengjin Xie", "Beijing University of Posts and Telecommunication, China", "");
+            add_listitem("Zhiwei Zhao", "University Electronic Science and Technology of China, China", "");
+            add_listitem("Tianzhang Xing", "Northwest University, China", "");
+            add_listitem("Hongju Cheng", "Fuzhou University, China", "");
+            add_listitem("Chuanwen Luo", "Beijing Forestry University, China", "");
+            add_listitem("En Wang", "Jilin University, China", "");
+            add_listitem("Zhichao Cao", "Michigan State University,USA", "");
+         add_listitem("Nadia Yoza", "National Institute of Standard and Technology Boulder,USA", "");
+      add_listitem("Arnob Ghosh", "New Jersey Insitute of Technology,USA", "");
+            add_listitem("Kai Li", "Northeastern University,USA", "");
+            add_listitem("Subhramoy Mohanti", "InterDigital Inc.,USA", "");
+            add_listitem("Xuyu Wang", "Florida International University,USA", "");
+            add_listitem("Tong Jian", "Analog Devices Inc.,USA", "");
+            add_listitem("Vini Chaudhury", "Northeastern University,USA", "");
+            add_listitem("Tiffany Zhang", "University of Nebraska Omaha,USA", "");
+
       ?>
 
       <li data-role="list-divider">Track 2: Edge Computing, IoT and Digital Twins </li>
@@ -20,6 +43,35 @@
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Xu Chen", "Sun Yat-sen University, China", "");
       add_listitem("Posco Tso", "Loughborough University, UK", "");
+
+ add_listitem("<b>TPC Members:</b>","","");
+
+            add_listitem("Yehia Elkhatib", "University of Glasgow, UK", "");
+            add_listitem("Chen Chen", "University of Cambridge,UK", "");
+            add_listitem("Gabor Kecskemeti", "University of Miskolc, Hungary", "");
+            add_listitem("Suresh Perinpanayagam", "University of York, UK", "");
+            add_listitem("Amirhosein Taherkordi", "University of Oslo, Norway", "");
+            add_listitem("Anish Jindal", "University of Durham, UK", "");
+            add_listitem("Lin Cui", "Jinan University, China", "");
+            add_listitem("Pedro Santos", "CISTER, Portugal", "");
+            add_listitem("Giuseppe Tricomi", "University of Messina, Italy", "");
+            add_listitem("Lars Nagel", "Loughborough University,UK", "");
+         add_listitem("Xiaoxi Zhang", "Sun Yat-sen University,China", "");
+add_listitem("Zhi Zhou", "Sun Yat-sen University,China", "");
+add_listitem("Haipeng Dai", "Nanjing University,China", "");
+add_listitem("Wei Cai", "The Chinese University of Hong Kong, Shenzhen,China", "");
+      add_listitem("Song Yang", "Beijing Institute of Technology,China", "");
+            add_listitem("Zehui Xiong", "Singapore University of Technology and Design,Singapore", "");
+            add_listitem("Lei Jiao", "University of Oregon,USA", "");
+            add_listitem("Songtao Guo", "Chongqing University,China", "");
+            add_listitem("Yue Cao", "Wuhan University,China", "");
+            add_listitem("Qiang He", "Huazhong University of Science and Technology,China", "");
+            add_listitem("Yupeng Li", "Hong Kong Baptist University,China", "");
+       add_listitem("Wei Bao", "University of Sydney,Australia", "");
+       add_listitem("Dinh C. Nguyen", "University of Alabama in Huntsville,USA", "");
+       add_listitem("Lei Yang", "University of Nevada, Reno,USA", "");
+       add_listitem("Jie Xu", "University of Miami,USA", "");
+       add_listitem("Xiaowen Gong", "Auburn University,USA", "");
       ?>
 
       
@@ -28,6 +80,25 @@
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Cliff C. Zou", "University of Central Florida, USA", "");
       add_listitem("Xiaoyu Ji", "Zhejiang University, China", "");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Shan Wang", "The Hong Kong Polytechnic University, HK", "");
+add_listitem("Chen Yan", "Zhejiang University, China", "");
+            add_listitem("Yushi Cheng", "Zhejiang University, China", "");
+            add_listitem("Guoming Zhang", "Shandong University, China", "");
+            add_listitem("Xinyan Zhou", "Ningbo University, China", "");
+            add_listitem("Yichao Chen", "Shanghaijiaotong University, China", "");
+            add_listitem("Pengfei Hu", "Shandong University, China", "");
+            add_listitem("Wei Wang", "Huazhong University of Science and Technology , China", "");
+            add_listitem("Lu Wang", "Shenzhen University, China", "");
+            add_listitem("Yuhong Nan", "Sun Yat-sen University, China", "");
+            add_listitem("Yan Meng", "Shanghai Jiao Tong University, China", "");
+            add_listitem("Yan Long", "University of Michigan,USA", "");
+         add_listitem("Daibo Liu", "Hunan University,China", "");
+      add_listitem("Jing Chen", "Wuhan University,China", "");
+            add_listitem("Zimin Zhao", "University at Buffalo,USA", "");
+            add_listitem("Jason (Minhui) Xue", "CSIRO’s Data61,Australia", "");
+
       ?>
 
       
@@ -36,6 +107,20 @@
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Tingjian Ge", "University of Massachusetts Lowell, USA", "");
       add_listitem("Tao Xiang", "Chongqing University, China", "");
+
+            add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Lei Feng", "Nanyang Technological University, Singapore", "");
+            add_listitem("Hongxin Wei", "Southern University of Science and Technology, China", "");
+            add_listitem("Yan Yan", "Carleton University, Canada", "");
+            add_listitem("Zhen Fang", "University of Technology Sydney, Australia", "");
+            add_listitem("Chaojie Wang", "Nanyang Technological University, Singapore", "");
+            add_listitem("Gengyu Lyu", "Beijing University of Technology, China", "");
+            add_listitem("Shuo He", "University of Electronic Science and Technology of China, China", "");
+            add_listitem("Yuzhou Cao", "Nanyang Technological University,Singapore", "");
+         add_listitem("Yidong Li", "Beijing Jiaotong University,China", "");
+      add_listitem("Fei Chen", "Shenzhen University,China", "");
+            add_listitem("Yushu Zhang", "Nanjing University of Aeronautics and Astronautics,China", "");
+            add_listitem("Ying Yang", "The Chinese University of Hong Kong,China", "");
       ?>
      
       <li data-role="list-divider">Track 5: Systems, Tools and Testbed </li>
@@ -43,6 +128,29 @@
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Kui Wu", "University of Victoria, Canada", "");
       add_listitem("Xiaolong Zheng", "Beijing University of Posts and Telecommunications, China", "");
+
+ add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("Helei Cui", "Northwestern Polytechnical University, China", "");
+add_listitem("YingYing Hou", "The Hong Kong Polytechnic University, China", "");
+            add_listitem("Pengfei Wang", "Dalian University of Technology, China", "");
+            add_listitem("Lu Wang", "Shenzhen University,China", "");
+                        add_listitem("Tongxin Zhu", "Southeast University, China", "");
+            add_listitem("Ruipeng Gao", "Beijing Jiaotong University, China", "");
+            add_listitem("Meng Jin", "Shanghai Jiaotong University, China", "");
+            add_listitem("Chenshu Wu", "The University of Hong Kong, China", "");
+            add_listitem("Meng Li", "Nanjing University, China", "");
+            add_listitem("Dan Xia", "Beijing University of Posts and Telecommunications, China", "");
+            add_listitem("Guomin Tang", "National University of Defence Technology , China", "");
+            add_listitem("Reza Malekian ", "Malmö University,Sweden", "");
+         add_listitem("Liangyi Gong", "Computer Network Information Center, Chinese Academy of Sciences,China", "");
+add_listitem("Chao Liu", "Ocean University of China,China", "");
+add_listitem("Dongxiao Yu", "Shandong University,China", "");
+add_listitem("Yifei Zou", "Shandong University,China", "");
+      add_listitem("Xin-Wei Yao", "Zhejiang University of Technology,China", "");
+            add_listitem("Jihong Yu", "Beijing Institute of Technology,China", "");
+            add_listitem("Qiben Yan", "Michigan State University,USA", "");
+            add_listitem("Jiang Liu", "Waseda University,Japan", "");
+
       ?>
 
       <li data-role="list-divider">Track 6: Smart Cities, Metaverse and Applications </li>
@@ -50,6 +158,34 @@
        add_listitem("<b>Track Chairs:</b>","","");
       add_listitem("Zichuan Xu", "Dalian University of Technology, China", "");
       add_listitem("Yue Zhang", "Drexel University, USA", "");
+
+add_listitem("<b>TPC Members:</b>","","");
+            add_listitem("JianLiang Wu", "Simon Fraser university ,Canada", "");
+add_listitem("Sanchuan Chen", "Fordham University, USA", "");
+            add_listitem("Ming Li", "Jinan University, China", "");
+            add_listitem("QingChuan Zhao", "City University of Hong Kong,China", "");
+            add_listitem("Xiaokuan Zhang", "George Mason University, USA", "");
+            add_listitem("Guoxing Chen", "Shanghai Jiao Tong University, China", "");
+            add_listitem("Ammar Hawbani", "University of Science and Technology of China, China", "");
+            add_listitem("Bryan Pearson", "Sandia National Laboratories, USA", "");
+            add_listitem("Ronny Hajoon Ko", "The Ohio State University, USA", "");
+            add_listitem("Ning Zhang", "Washington University in St. Louis, USA", "");
+            add_listitem("Song Li", "Zhejiang University, China", "");
+            add_listitem("Yang Liu", "Xidian University,China", "");
+         add_listitem("Hao Wu", "Nanjing University,China", "");
+add_listitem("Biao Han", "National University of Defense Technology,China", "");
+add_listitem("Marco Brocanelli", "Wayne State University ,USA", "");
+add_listitem("Qiufen Xia", "Dalian University of Technology, China", "");
+      add_listitem("Jiankang Ren", "Dalian University of Technology,China", "");
+            add_listitem("Haolin Liu", "Xiangtan University,China", "");
+            add_listitem("Jing Li", "The Hong Kong Polytechnic University,China", "");
+            add_listitem("Ruiting Zhou", "Southeast University,China", "");
+            add_listitem("Weifa Liang", "City University of Hong Kong,China", "");
+            add_listitem("Wenzheng Xu", "Sichuan University,China", "");
+            add_listitem("Pan Zhou", "Huazhong University and Science and Technology,China", "");
+       add_listitem("Feng Lin", "Zhejiang University,China", "");
+       add_listitem("Song Yang", "Beijing Institute of Technology,China", "");
+
       ?>
 
     </ul>
