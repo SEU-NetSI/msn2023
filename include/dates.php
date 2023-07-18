44f7bbb67de739c892f103d07746b7a1513e32ce
Iven0322 <103888165+Iven0322@users.noreply.github.com>
2023-07-18 15:42:45 +0800

    Update dates.php

--- include/dates.php
+++ include/dates.php
@@ -1,6 +1,6 @@
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
         <?php add_dateitem("<del>Jul 20, 2023<del>", "<del>Paper Submission Due<del>"); ?>
-    <?php add_dateitem("<span style=\"color: red;font-weight: bold\">Aug 5, 2023</span>", "<span style=\"color: red;font-weight: bold\">Paper Submission Due(extended)</span>"); ?>
+    <?php add_dateitem("<span style=\"color: red;font-weight: bold\">Aug 5, 2023</span>", "<span style=\"color: red;font-weight: bold\">Paper Submission Due (extended)</span>"); ?>
 
     <?php add_dateitem("Oct. 1, 2023", "Author Notification"); ?>
 
