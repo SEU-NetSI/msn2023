ec0d857a6670e2ea88c1782a077edc139a0067f6
Iven0322 <103888165+Iven0322@users.noreply.github.com>
2023-08-08 11:22:02 +0800

    划掉call for paper里important days中的截稿日期

--- cf-papers.php
+++ cf-papers.php
@@ -50,7 +50,7 @@
 
 		<ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
 		   <?php add_dateitem("<del>Jul 20, 2023<del>", "<del>Paper Submission Due<del>"); ?>
-    <?php add_dateitem("<span style=\"color: red;font-weight: bold\">Aug 5, 2023</span>", "<span style=\"color: red;font-weight: bold\">Paper Submission Due (extended)</span>"); ?>
+    <?php add_dateitem("<del><span style=\"color: red;font-weight: bold\">Aug 5, 2023</span><del>", "<del><span style=\"color: red;font-weight: bold\">Paper Submission Due (extended)</span><del>"); ?>
     		<?php add_dateitem("Oct. 1, 2023", "Author Notification"); ?>
     	
 		    <?php add_dateitem("Oct 25, 2023", "Camera-Ready Due"); ?>
