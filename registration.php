95ab4d8b05cdf1da3fa7c1bd5de24804345f8608
Iven0322 <103888165+Iven0322@users.noreply.github.com>
2023-10-12 16:48:46 +0800

    Update registration.php(更新了registration网页信息）

--- registration.php
+++ registration.php
@@ -6,7 +6,116 @@
 <div data-role="content" style="text-align:left;padding-top: 0em;background-color:#e4e5e6">
     <div  class="ui-block-a" style="width:70%;height:100%;background-color:#ffffff;padding-left: 35px;padding-right:35px;padding-top: 15px;">
         <h1>Registration</h1>
-        <p>TBD</p>
+           <p>Online registration is now available. All participants must register and pay the appropriate fee in order to attend MSN 2023.</p>
+   <p>Before registering for the conference, please read the following policies and procedures on this page. If you have finished reading this page and are ready to start the registration process, please click on the button below.<p>
+   
+<h2>Author Registration Requirements</h2>
+   <ul>
+       <li><b>Every accepted paper</b> (in the conference/ workshop) must have one author register at the Author Registration rate, even if the author is a student, <b>by 25 October 2023</b>, and presented by the author at the conference. </li>
+
+       <li>Each author registration only covers one accepted paper. Authors with multiple papers must either pay for multiple registrations or ask their co-authors to register for the other papers. </li>
+
+       <li>Papers without registered author(s) will not be included in the conference program and proceedings.</li>
+   </ul>
+
+
+     <h2>Registration Fees</h2>
+
+   <b style="color:rgb(46,116,181)">Author Registration - <span style="color:red">Deadline: 25 October 2023</span></b><br><br>
+
+   <table border="1" style="border-collapse: collapse;">
+       <tr>
+           <th style="text-align: center;" rowspan="2">Registration Type</th>
+           
+       </tr>
+       <tr>
+           <th style="text-align: center;">&nbsp; &nbsp;&nbsp; &nbsp;Fee (USD)&nbsp; &nbsp;&nbsp; &nbsp;</th>
+           <th style="text-align: center;">&nbsp; &nbsp;&nbsp; &nbsp;Fee (CNY)&nbsp; &nbsp;&nbsp; &nbsp;</th>
+       </tr>
+       <tr>
+           <td>IEEE Member</td>
+           <td style="text-align: center;">650</td>
+           <td style="text-align: center;">4800</td>
+       </tr>
+       <tr>
+           <td>Non-IEEE Member &nbsp; &nbsp; &nbsp; &nbsp;</td>
+           <td style="text-align: center;">700</td>
+           <td style="text-align: center;">5200</td>
+       </tr>
+       
+   </table>
+
+
+    <p><b style="color:rgb(46,116,181)">Conference Registration (for non-authors and non-presenting authors)</b></p>
+
+
+    <table border="1" style="border-collapse: collapse;">
+       <tr>
+           <th style="text-align: center;" rowspan="2">Registration Type</th>
+
+       </tr>
+       <tr>
+           <th style="text-align: center;">&nbsp; &nbsp;&nbsp; &nbsp;Fee (USD)&nbsp; &nbsp;&nbsp; &nbsp;</th>
+           <th style="text-align: center;">&nbsp; &nbsp;&nbsp; &nbsp;Fee (CNY)&nbsp; &nbsp;&nbsp; &nbsp;</th>
+       </tr>
+       <tr>
+           <td>IEEE Member</td>
+           <td style="text-align: center;">650</td>
+           <td style="text-align: center;">4800</td>
+       </tr>
+       <tr>
+           <td>Non-IEEE Member &nbsp; &nbsp; &nbsp; &nbsp;</td>
+           <td style="text-align: center;">700</td>
+           <td style="text-align: center;">5200</td>
+       </tr>
+
+       <tr>
+           <td>IEEE Student Member</td>
+           <td style="text-align: center;">400</td>
+           <td style="text-align: center;">3000</td>
+       </tr>
+       <tr>
+           <td>Non-IEEE Student Member</td>
+           <td style="text-align: center;">450</td>
+           <td style="text-align: center;">3300</td>
+       </tr>
+   </table>
+
+
+   <p><b>Author registrations</b> include: access to the conference venue, 3 daily coffee breaks, 1 lunch per day, 1 ticket for the banquet, 1 ticket for the dinner, presentation and publication of one paper (the registrant must be a co-author of the associated paper).</p>
+   <p><b>Conference registrations</b> include: access to the conference venue, 3 daily coffee breaks, 1 lunch per day, 1 ticket for the banquet, 1 ticket for the dinner.</p>
+
+    <h2>Registration Procedure</h2>
+   <p>Registrations are handled only in electronic form. Only fully completed registration forms will be accepted. The registration fee is based on the date of the receipt of the registration form and the payment in full, in accordance with the deadlines mentioned above. The registration will only be confirmed upon receipt of payment in full. </p>
+   <p>Each registrant should send the transfer receipt and registration form (<a href="form.docx">Chinese version registration form for the participants from the mainland of China</a>  and <a href="MSN 2023 International Conference Registration Form 1.xlsx">English version for the ones from other areas and countries</a> ) to <b>ieee.msn2023@gmail.com</b> with subject "MSN Paper Registration - paper ID" (your paper ID can be checked out from EasyChair). If you have any questions regarding registration, please feel free to contact us via <b>ieee.msn2023@gmail.com</b>.<p>
+
+   <h2>Payment</h2>
+   <p>All registrations must be submitted online and paid by credit card. </p>
+
+   <h2>Terms and Policies</h2>
+
+   <p><u><i>Registration Policies for Authors</i></u></p>
+   <ul>
+       <li>Each Author Registration is valid for ONE paper.</li>
+       <li>To be published in the MSN 2023 Conference Proceedings and to be eligible for publication in IEEE Xplore, at least one author of an accepted paper is required to register for the conference at the Author Registration Rate even if the author is a student, and present the paper at the conference.</li>
+       <li>The accepted paper must be presented by an author of that paper at the conference.</li>
+       <li>Papers without registered author(s) by 25 October 2023 will not be included in the conference program and proceedings.</li>
+   </ul>
+   <p><u><i>Registration Policies for All Attendees</i></u></p>
+    <p>Each attendee must have a distinct registration. Registrations are not transferable.</p>
+    p><u><i>No-show Policies </i></u></p>
+   <p>Each paper must be presented by an author of the paper at the conference. An accepted paper not presented by one of the authors will be removed from the final conference proceedings. No refund will be made to authors of these papers.</p>
+
+   p><u><i>Cancellation Policies</i></u></p>
+   <ul>
+       <li>All cancellation requests must be received in writing to <a href="mailto:carmen.au@polyu.edu.hk">carmen.au@polyu.edu.hk</a> <b>by 14 November 2023.</b> No refund will be provided after this date. There will be <b>an administrative fee of US$ 90(CNY 700) </b>deducted from each cancellation.</li>
+       <li><b>Author registration is not refundable.</b></li>
+       <li>If payment is not received, registration will be automatically cancelled.</li>
+   </ul>
+
+    <p><u><i>Privacy Policy</i></u></p>
+   <p>By registering for the conference, relevant details will be incorporated into a participant list for the benefit of all delegates. Normally this information would be: first name, last name, institution, country and email address. These details may also be available to parties directly related to the conference organization including the venues and accommodation providers. We may use these details to inform current participants of conference updates or future conferences via email. Participants are responsible for advising us if they do not wish to have their email addresses included in the conference participant list or distribution list for future events.</p>
+
         
     
         <br><br><br><br><br>
