cae347e1a92135d92df27550fe779f8705a84096
Iven0322 <1584594639@qq.com>
2023-10-26 21:29:54 +0800

    修改日期

--- cam-submission.php
+++ cam-submission.php
@@ -46,8 +46,8 @@
           <h2>Important Deadlines</h2>
 <ul data-role="listview" data-inset="true" data-theme="a" data-content-theme="a" class="datetbl">
     <?php add_dateitem("<b>30 October 2023</b>", "Camera ready"); ?>
-    <?php add_dateitem("<b>25 October 2023</b>", "Copyright"); ?>
-    <?php add_dateitem("<b>25 October 2023</b>", "Registration"); ?>
+    <?php add_dateitem("<b>30 October 2023</b>", "Copyright"); ?>
+    <?php add_dateitem("<b>30 October 2023</b>", "Registration"); ?>
 </ul>
 
     <h2>Submission of the Camera Ready and Copyright Form</h2>
