3884a4f5cb3afbe84edf99dca1cfd2a36f687139
Iven0322 <1584594639@qq.com>
2023-12-08 22:50:16 +0800

    增加了booklet

--- glance.php
+++ glance.php
@@ -5,7 +5,7 @@
 <div data-role="content" style="text-align:left;padding-top: 0em;background-color:#e4e5e6">
     <div  class="ui-block-a" style="width:100%;height:100%;background-color:#ffffff;padding-left: 15px;padding-right:15px;padding-top: 15px;">
         <h1 style="text-align: center">Program at a Glance</h1>
-        <!-- <p style="text-align: center">Conference Program and Information Booklet is available<a href="files/booklet.pdf" target="_blank"> here</a>.</p> -->
+        <p style="text-align: center">Conference Program and Information Booklet is available<a href="files/msn 2023 booklet.pdf" target="_blank"> here</a>.</p>
 
         <img style="width:100%;" src="images/program/pro2023.jpg">
         <br><br><br>
